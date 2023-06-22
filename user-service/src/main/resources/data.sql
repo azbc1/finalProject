4a7c78c51b53abf8d04396e6255994b9521f4011
Ghassen FILALI <gfilali@vermeg.com>
2023-06-22 21:34:29 +0100

    Add authentication passwords

--- user-service/src/main/resources/data.sql
+++ user-service/src/main/resources/data.sql
@@ -8,10 +8,10 @@ MERGE INTO roles (id, name) VALUES
 MERGE INTO users (id, organization_id, site_id, name, email, phone, position, username, password)
 VALUES
   (1, 1, 1, 'Sayed Mohamed', 'sayed@test.com', '010123456789', 'Manager', 'sayed', '$2a$12$V2IuTBSRrf7aRW6rQd9NgO6.Y8ghDcYIBS9wTeTme1gwvMw4hHy/q'),
-  (2, 1, 2, 'Ahmed Mahmoud', 'ahmed@teat.com', '01097654321', 'Developer', 'ahmed', 'password2'),
-  (3, 2, 3, 'Mohammad Ali', 'mohammad@test.com', '010678954321', 'Analyst', 'mohammad', 'password3'),
-  (4, 3, 4, 'Hamada Ibrahim', 'hamada@test.com', '010531247896', 'Developer', 'hamada', 'password4'),
-  (5, 2, 3, 'Tamer Hassen', 'tamer@test.com', '010975874321', 'Analyst', 'tamer', 'password5');
+  (2, 1, 2, 'Ahmed Mahmoud', 'ahmed@teat.com', '01097654321', 'Developer', 'ahmed', '$2a$12$FU.Wpyz4sReCNuGO6ht.IufVk0JzQqbUodgXqRn1Uh0/Nco/OnnaS'),
+  (3, 2, 3, 'Mohammad Ali', 'mohammad@test.com', '010678954321', 'Analyst', 'mohammad', '$2a$12$fICaEBqnTuxoQb6nm8kmnOSHyFvOSmrGfNWvoWhoVI62byI9avJjW'),
+  (4, 3, 4, 'Hamada Ibrahim', 'hamada@test.com', '010531247896', 'Developer', 'hamada', '$2a$12$lHkg5NDv6qJ9HKCWSVxgV.gNRQgf09L6Z5FJcUSgxesetitZwxRwm'),
+  (5, 2, 3, 'Tamer Hassen', 'tamer@test.com', '010975874321', 'Analyst', 'tamer', '$2a$12$nX328Dw5DBr9/6WFIbP9neHGRZqIUYhjqwBpukEPaAi07Oqsg.SGK');
 
 -- Associate roles with users
 INSERT INTO user_roles (user_id, role_id)
