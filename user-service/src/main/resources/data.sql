8a9931f1919559704c92f7ad6603e78211f00ffe
gfilaliEsprit <87043187+gfilaliEsprit@users.noreply.github.com>
2023-06-23 21:45:19 +0100

    Update data.sql

--- user-service/src/main/resources/data.sql
+++ user-service/src/main/resources/data.sql
@@ -11,9 +11,10 @@ VALUES
   (2, 1, 2, 'Ahmed Mahmoud', 'ahmed@teat.com', '01097654321', 'Developer', 'ahmed', '$2a$12$FU.Wpyz4sReCNuGO6ht.IufVk0JzQqbUodgXqRn1Uh0/Nco/OnnaS'),
   (3, 2, 3, 'Mohammad Ali', 'mohammad@test.com', '010678954321', 'Analyst', 'mohammad', '$2a$12$fICaEBqnTuxoQb6nm8kmnOSHyFvOSmrGfNWvoWhoVI62byI9avJjW'),
   (4, 3, 4, 'Hamada Ibrahim', 'hamada@test.com', '010531247896', 'Developer', 'hamada', '$2a$12$lHkg5NDv6qJ9HKCWSVxgV.gNRQgf09L6Z5FJcUSgxesetitZwxRwm'),
-  (5, 2, 3, 'Tamer Hassen', 'tamer@test.com', '010975874321', 'Analyst', 'tamer', '$2a$12$nX328Dw5DBr9/6WFIbP9neHGRZqIUYhjqwBpukEPaAi07Oqsg.SGK');
+  (5, 2, 3, 'Tamer Hassen', 'tamer@test.com', '010975874321', 'Analyst', 'tamer', '$2a$12$nX328Dw5DBr9/6WFIbP9neHGRZqIUYhjqwBpukEPaAi07Oqsg.SGK'),
+  (6, 1, 5, 'Sayed Mohamed', 'sayed@test.com', '010123456789', 'Manager', 'admin', '$2a$12$0UG2jkLjPlQD76DxGzPoT.06OOOoMxdzOHOAdJRl4/2YNaHRldO7m');
 
 -- Associate roles with users
 INSERT INTO user_roles (user_id, role_id)
 VALUES
-  (1, 1), (2, 1), (3, 1), (4, 1), (5, 1); -- Assigning 'ROLE_ADMIN' to all users
+  (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1); -- Assigning 'ROLE_ADMIN' to all users
