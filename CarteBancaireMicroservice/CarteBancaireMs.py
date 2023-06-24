8082b298d86069ac768a6ca1055ae98d56cebd2b
aya1409 <aya.fathallah@esprit.tn>
2023-06-24 10:23:48 +0100

    pyhthon prject with front

---
+++ CarteBancaireMicroservice/CarteBancaireMs.py
@@ -0,0 +1,97 @@
+from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS
+import psycopg2
+import logging
+from py_eureka_client import eureka_client
+
+app = Flask(__name__)
+CORS(app)
+
+# Database connection parameters
+db_params = {
+    'host': '172.27.0.2',
+    'port': 5432,
+    'database': 'cartecredit',
+    'user': 'postgres',
+    'password': 'Postgres+123'
+}
+
+# Connect to the PostgreSQL database
+conn = psycopg2.connect(**db_params)
+cursor = conn.cursor()
+
+# Create the cards table if it doesn't exist
+create_table_query = '''
+    CREATE TABLE IF NOT EXISTS cards (
+        id SERIAL PRIMARY KEY,
+        card_number VARCHAR(16) NOT NULL,
+        card_holder VARCHAR(255) NOT NULL,
+        expiry_date VARCHAR(10) NOT NULL
+    )
+'''
+cursor.execute(create_table_query)
+conn.commit()
+
+# Configure Eureka client
+eureka_client.init(
+    eureka_server="http://localhost:8761/eureka/",
+    app_name="MyApplication",
+    instance_host="127.0.0.2",
+    instance_port=5000,
+    zone="default",
+    prefer_same_zone=True
+)
+
+@app.route('/api/python/cards', methods=['GET'])
+def get_all_cards():
+    query = "SELECT * FROM cards"
+    cursor.execute(query)
+    cards = cursor.fetchall()
+    return jsonify(cards)
+
+@app.route('/api/python/create', methods=['POST'])
+def add_card():
+    card = request.get_json()
+    query = "INSERT INTO cards (card_number, card_holder, expiry_date) VALUES (%s, %s, %s)"
+    values = (card['card_number'], card['card_holder'], card['expiry_date'])
+    cursor.execute(query, values)
+    conn.commit()
+    return jsonify({'message': 'Card added successfully.'})
+
+@app.route('/cards/<int:card_id>', methods=['GET'])
+def get_card(card_id):
+    query = "SELECT * FROM cards WHERE id = %s"
+    values = (card_id,)
+    cursor.execute(query, values)
+    card = cursor.fetchone()
+    if card:
+        return jsonify(card)
+    return jsonify({'message': 'Card not found.'}), 404
+
+@app.route('/cards/<int:card_id>', methods=['PUT'])
+def update_card(card_id):
+    updated_card = request.get_json()
+    query = "UPDATE cards SET card_number = %s, card_holder = %s, expiry_date = %s WHERE id = %s"
+    values = (updated_card['card_number'], updated_card['card_holder'], updated_card['expiry_date'], card_id)
+    cursor.execute(query, values)
+    conn.commit()
+    if cursor.rowcount > 0:
+        return jsonify({'message': 'Card updated successfully.'})
+    return jsonify({'message': 'Card not found.'}), 404
+
+@app.route('/cards/<int:card_id>', methods=['DELETE'])
+def delete_card(card_id):
+    query = "DELETE FROM cards WHERE id = %s"
+    values = (card_id,)
+    cursor.execute(query, values)
+    conn.commit()
+    if cursor.rowcount > 0:
+        return jsonify({'message': 'Card deleted successfully.'})
+    return jsonify({'message': 'Card not found.'}), 404
+
+@app.route('/api/python/front')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
